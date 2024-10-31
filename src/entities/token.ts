1a8020353b9be16baf476ac4ede2648b1cd84b94
Nusantara <mumibiruinferno@gmail.com>
2024-10-31 18:11:42 +0700

    update sdk

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -59,16 +59,16 @@ export function currencyEquals(currencyA: Currency, currencyB: Currency): boolea
 export const WETH = {
   [ChainId.MAINNET]: new Token(
     ChainId.MAINNET,
-    '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    '0x5b8361a91d33Ad1a9541832B2B60b1CEd2D04148',
     18,
-    'WBNB',
-    'Wrapped BNB'
+    'WDNY',
+    'Wrapped DNY'
   ),
   [ChainId.BSCTESTNET]: new Token(
     ChainId.BSCTESTNET,
     '0xaE8E19eFB41e7b96815649A6a60785e1fbA84C1e',
     18,
-    'WBNB',
-    'Wrapped BNB'
+    'WDNY',
+    'Wrapped Dynasty'
   )
 }
